9fec607bb07ba0a158d2fac843f841609bd659e1
Dan Anderson <anderson@familymedia.org>
2019-09-22 17:01:12 -0400

    Static Mockup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,239 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+  <head>
+    <meta charset="utf-8">
+    <title>Laravel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
+    <link rel="stylesheet" type="text/css" media="all" href="{{ asset('css/app.css') }}" />
+  </head>
+  <body>
+    <div id="wrapper">
+      <main>
+        <div class="row">
+          <div class="col-6">
+            <div class="asset-box tall">
+              <div class="asset-image">
+                <a href="#" target="_self" style="background-image: url(images/phoenix.jpg)" title=""></a>
+              </div>
+              <div class="asset-content">
+                <header>
+                  <h1><a href="#">Find the Perfect Dress for Special Occasions</a></h1>
+                </header>
+                <footer>
+                  <a href="#" class="button" style="background-image:url(images/sanders.jpg)"></a>
+                  <ul>
+                    <li>Jennifer Nicole Sullivan</li>
+                    <li>Vintage Clothing Expert</li>
+                  </ul>
+                  <a href="#" class="button share"></a>
+                </footer>
+              </div>
             </div>
+          </div>
+          <div class="col-6">
+            <div class="asset-box">
+              <div class="asset-image">
+                <a href="#" target="_self" style="background-image: url(images/batman.jpg)" title=""></a>
+              </div>
+              <div class="asset-content">
+                <header>
+                  <h1><a href="#">Attracting Butterflies to Your Garden</a></h1>
+                  <a href="#" class="button share"></a>
+                </header>
+                <footer>
+                  <a href="#" class="button" style="background-image:url(images/sanders.jpg)"></a>
+                  <ul>
+                    <li>Marie Iannotti</li>
+                    <li>Gardening Expert</li>
+                  </ul>
+                </footer>
+              </div>
+            </div>
+            <div class="asset-box">
+              <div class="asset-image">
+                <a href="#" target="_self" style="background-image: url(images/daredevil.jpg)" title=""></a>
+              </div>
+              <div class="asset-content">
+                <header>
+                  <h1><a href="#">The Absolute Beginner's Guide to Running</a></h1>
+                  <a href="#" class="button share"></a>
+                </header>
+                <footer>
+                  <a href="#" class="button" style="background-image:url(images/sanders.jpg)"></a>
+                  <ul>
+                    <li>Christine Luff</li>
+                    <li>Running Expert</li>
+                  </ul>
+                </footer>
+              </div>
+            </div>
+          </div>
         </div>
-    </body>
+        <div class="row">
+          <div class="col-6">
+            <div class="asset-box">
+              <div class="asset-image">
+                <a href="#" target="_self" style="background-image: url(images/hulk.jpg)" title=""></a>
+              </div>
+              <div class="asset-content">
+                <header>
+                  <h1><a href="#">DIY Live Edge Coffee Table</a></h1>
+                  <a href="#" class="button share"></a>
+                </header>
+                <footer>
+                  <a href="#" class="button" style="background-image:url(images/sanders.jpg)"></a>
+                  <ul>
+                    <li>Staphanie White</li>
+                    <li>DIY Expert</li>
+                  </ul>
+                </footer>
+              </div>
+            </div>
+            <div class="asset-box">
+              <div class="asset-image">
+                <a href="#" target="_self" style="background-image: url(images/squirell-girl.jpg)" title=""></a>
+              </div>
+              <div class="asset-content">
+                <header>
+                  <h1><a href="#">Best Free Attractions in New York City</a></h1>
+                  <a href="#" class="button share"></a>
+                </header>
+                <footer>
+                  <a href="#" class="button" style="background-image:url(images/sanders.jpg)"></a>
+                  <ul>
+                    <li>Heather Cross</li>
+                    <li>New York City Travel Expert</li>
+                  </ul>
+                </footer>
+              </div>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="asset-box">
+              <div class="asset-image">
+                <a href="#" target="_self" style="background-image: url(images/spiderman.jpg)" title=""></a>
+              </div>
+              <div class="asset-content">
+                <header>
+                  <h1><a href="#">Italian Sausage and Bell Pepper Pizza</a></h1>
+                  <a href="#" class="button share"></a>
+                </header>
+                <footer>
+                  <a href="#" class="button" style="background-image:url(images/sanders.jpg)"></a>
+                  <ul>
+                    <li>Will Forte</li>
+                    <li>Last Man On Earch</li>
+                  </ul>
+                </footer>
+              </div>
+            </div>
+            <div class="asset-box">
+              <div class="asset-image">
+                <a href="#" target="_self" style="background-image: url(images/wolverine.jpg)" title=""></a>
+              </div>
+              <div class="asset-content">
+                <header>
+                  <h1><a href="#">10 Mid Century Moden Items for Your Home</a></h1>
+                  <a href="#" class="button share"></a>
+                </header>
+                <footer>
+                  <a href="#" class="button" style="background-image:url(images/sanders.jpg)"></a>
+                  <ul>
+                    <li>Jessica Jones</li>
+                    <li>Private Detective</li>
+                  </ul>
+                </footer>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <aside>
+        <div class="ad-block">
+          <div>
+            <a href="https://www.starbucks.com/" target="_blank"><img src="images/advert.jpg" /></a>
+          </div>
+        </div>
+        <div class="module list">
+          <h3>Trending</h3>
+          <div class="row">
+            <div class="col-6">
+              <span class="image-wrap">
+                <a href="#" class="image"><img src="images/vulture.jpg"/></a>
+                <a href="#" class="button" style="background-image:url(images/sanders.jpg)"></a>
+              </span>
+            </div>
+            <div class="col-6">
+              <h4><a href="#">3 Questions to Answer Before You Buy Your First Investment</a></h4>
+              <footer>
+                <a href="#" class="button" style="background-image:url(images/sanders.jpg)"></a>
+                <ul>
+                  <li>By Joshua Kennon</li>
+                  <li>Investing for Geninners...</li>
+                </ul>
+                <a href="#" class="button share"></a>
+              </footer>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <span class="image-wrap">
+                <a href="#" class="image"><img src="images/wolverine-face.jpg"/></a>
+                <a href="#" class="button" style="background-image:url(images/sanders.jpg)"></a>
+              </span>
+            </div>
+            <div class="col-6">
+              <h4><a href="#">Pairing Food and Wine</a></h4>
+              <footer>
+                <a href="#" class="button" style="background-image:url(images/sanders.jpg)"></a>
+                <ul>
+                  <li>By Stacy Slinkard</li>
+                  <li>Wine Expert</li>
+                </ul>
+                <a href="#" class="button share"></a>
+              </footer>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <span class="image-wrap">
+                <a href="#" class="image"><img src="images/fantastic.jpg"/></a>
+                <a href="#" class="button" style="background-image:url(images/sanders.jpg)"></a>
+              </span>
+            </div>
+            <div class="col-6">
+              <h4><a href="#">12 Questions to Expect on Your College Interview</a></h4>
+              <footer>
+                <a href="#" class="button" style="background-image:url(images/sanders.jpg)"></a>
+                <ul>
+                  <li>By Allen Grove</li>
+                  <li>College Admissions Expert</li>
+                </ul>
+                <a href="#" class="button share"></a>
+              </footer>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-6">
+              <span class="image-wrap">
+                <a href="#" class="image"><img src="images/saga.jpg"/></a>
+                <a href="#" class="button" style="background-image:url(images/sanders.jpg)"></a>
+              </span>
+            </div>
+            <div class="col-6">
+              <h4><a href="#">How Not to Look Fat in White Pants</a></h4>
+              <footer>
+                <a href="#" class="button" style="background-image:url(images/sanders.jpg)"></a>
+                <ul>
+                  <li>By Cynthia Nellis</li>
+                  <li>Fashion Expert</li>
+                </ul>
+                <a href="#" class="button share"></a>
+              </footer>
+            </div>
+          </div>
+        </div>
+      </aside>
+    </div>
+  </body>
 </html>
