69ba71574f483ef562ac70d0763ea0ca47c9ab13
Dan Anderson <anderson@familymedia.org>
2019-09-22 21:00:06 -0400

    Fresh Articles (Generated)

---
+++ database/migrations/2019_09_22_221744_create_articles_tables.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateArticlesTables extends Migration
+{
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            createDefaultTableFields($table, true, true, true);
+            $table->integer('position')->unsigned()->nullable();
+        });
+
+        Schema::create('article_translations', function (Blueprint $table) {
+            createDefaultTranslationsTableFields($table, 'article');
+            $table->string('title')->nullable();
+            $table->text('content')->nullable();
+        });
+
+        Schema::create('article_slugs', function (Blueprint $table) {
+            createDefaultSlugsTableFields($table, 'article');
+        });
+
+        Schema::create('article_revisions', function (Blueprint $table) {
+            createDefaultRevisionsTableFields($table, 'article');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('article_revisions');
+        Schema::dropIfExists('article_translations');
+        Schema::dropIfExists('article_slugs');
+        Schema::dropIfExists('articles');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <title>Area 17 :: Front End :: Home</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" type="text/css" media="all" href="{{ asset('stylesheets/normalize.css') }}" />
