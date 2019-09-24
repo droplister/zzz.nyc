73cec012b7123fe98564952dec42587d4b3f4b4a
Dan Anderson <anderson@familymedia.org>
2019-09-23 21:45:13 -0400

    Features -> Homepage in CMS Nav

--- config/twill-navigation.php
+++ config/twill-navigation.php
@@ -2,7 +2,7 @@
 
 return [
     'featured' => [
-        'title' => 'Features',
+        'title' => 'Homepage',
         'route' => 'admin.featured.homepage',
         'primary_navigation' => [
             'homepage' => [
