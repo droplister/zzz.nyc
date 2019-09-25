84a4948694eaddc0e32d796e94ae7fcd5817348c
Dan Anderson <anderson@familymedia.org>
2019-09-25 04:24:51 -0400

    Author Columns

--- app/Http/Controllers/Admin/AuthorController.php
+++ app/Http/Controllers/Admin/AuthorController.php
@@ -7,4 +7,25 @@ use A17\Twill\Http\Controllers\Admin\ModuleController;
 class AuthorController extends ModuleController
 {
     protected $moduleName = 'authors';
+
+    /*
+     * Available columns of the index view
+     */
+    protected $indexColumns = [
+        'image' => [
+            'thumb' => true, // image column
+            'variant' => [
+                'role' => 'profile',
+                'crop' => 'default',
+            ],
+        ],
+        'title' => [ // field column
+            'title' => 'Title',
+            'field' => 'title',
+        ],
+        'bio' => [ // field column
+            'title' => 'Bio',
+            'field' => 'description',
+        ],
+    ];
 }
